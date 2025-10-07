79b33043b989faf80cb6fda142f262747c65b326
Digital-Ital <Djillali.Dev@gmail.com>
2025-10-07 15:39:38 -0400

    Add category URLs: /categories/slug routes, category show pages, linkable category tags

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+  def show
+    @category = Category.find_by!(slug: params[:id])
+    @featured_playlists = Playlist.featured.ordered.limit(4)
+    @main_families = Category.main_families.includes(children: :children)
+    
+    # For the tree view - we'll highlight this category
+    @current_category_id = @category.id
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Category not found"
+  end
+end
