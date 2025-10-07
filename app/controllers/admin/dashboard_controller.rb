5ecb62c6b12a9df18da5348449334af94a95887b
Digital-Ital <Djillali.Dev@gmail.com>
2025-10-06 22:03:03 -0400

    feat: create comprehensive admin dashboard with stats, navigation, and improved UI

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,14 +2,9 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @categories_count = Category.count
     @playlists_count = Playlist.count
-    @uncategorized_count = Playlist.where(category_id: nil).count
-  end
-end
-
-class Admin::DashboardController < Admin::BaseController
-  def index
-    @categories_count = Category.count
-    @playlists_count = Playlist.count
-    @uncategorized_count = Playlist.where(category_id: nil).count
+    @uncategorized_count = Playlist.left_joins(:categories).where(categories: { id: nil }).count
+    @featured_count = Playlist.where(featured: true).count
+    @root_categories = Category.where(parent_id: nil).count
+    @subcategories = Category.where.not(parent_id: nil).count
   end
 end
