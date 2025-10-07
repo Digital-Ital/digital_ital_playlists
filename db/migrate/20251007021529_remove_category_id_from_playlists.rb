7a535456a52170438fbbae1c4b0d062007a7bff4
Digital-Ital <Djillali.Dev@gmail.com>
2025-10-06 22:30:58 -0400

    fix: correct migration to remove category_id column properly

--- db/migrate/20251007021529_remove_category_id_from_playlists.rb
+++ db/migrate/20251007021529_remove_category_id_from_playlists.rb
@@ -1,6 +1,6 @@
 class RemoveCategoryIdFromPlaylists < ActiveRecord::Migration[8.0]
   def change
     remove_foreign_key :playlists, :categories if foreign_key_exists?(:playlists, :categories)
-    remove_reference :playlists, :category_id, null: false, foreign_key: false
+    remove_column :playlists, :category_id if column_exists?(:playlists, :category_id)
   end
 end
