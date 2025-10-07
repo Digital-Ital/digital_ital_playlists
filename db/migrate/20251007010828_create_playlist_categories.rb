dd9dac2af2e90a14a35f629aa3f4a20de47f117d
Digital-Ital <Djillali.Dev@gmail.com>
2025-10-06 21:29:58 -0400

    fix: make playlist_categories migration safe with if_not_exists to avoid duplicate table errors

--- db/migrate/20251007010828_create_playlist_categories.rb
+++ db/migrate/20251007010828_create_playlist_categories.rb
@@ -1,12 +1,12 @@
 class CreatePlaylistCategories < ActiveRecord::Migration[8.0]
   def change
-    create_table :playlist_categories do |t|
+    create_table :playlist_categories, if_not_exists: true do |t|
       t.references :playlist, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
 
-    add_index :playlist_categories, [:playlist_id, :category_id], unique: true
+    add_index :playlist_categories, [:playlist_id, :category_id], unique: true, if_not_exists: true
   end
 end
