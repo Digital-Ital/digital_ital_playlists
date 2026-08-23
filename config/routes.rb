16c1e88910150b93df1c4daaf2d37a7face54621
Digital-Ital <digital.ital.soundsystem@gmail.com>
2026-08-23 11:10:00 -0400

    Add Listening Desk routes without unrelated formatting

--- config/routes.rb
+++ config/routes.rb
@@ -7,31 +7,31 @@ Rails.application.routes.draw do
   get "all-songs", to: "pages#all_songs", as: :all_songs
   get "listening", to: "listening#show", as: :listening
 
-  namespace :admin do
-    root to: "dashboard#index"
-    get "listening/connect", to: "listening#authorize", as: :listening_authorize
-    get "listening/spotify/callback", to: "listening#callback", as: :listening_spotify_callback
-    resources :categories
-    resources :playlists do
-      collection do
-        post :import_spotify
-        post :start_batch_update
-        get :batch_update_progress
+      namespace :admin do
+        root to: "dashboard#index"
+        get "listening/connect", to: "listening#authorize", as: :listening_authorize
+        get "listening/spotify/callback", to: "listening#callback", as: :listening_spotify_callback
+        resources :categories
+        resources :playlists do
+          collection do
+            post :import_spotify
+            post :start_batch_update
+            get :batch_update_progress
+          end
+          member do
+            post :sync_with_spotify
+          end
+        end
+        resources :update_logs, only: [ :index ]
+        resources :share_events, only: [ :index ]
+        resources :spotify_opens, only: [ :index ]
+        resources :batch_updates, only: [ :index, :show ]
+        get "analytics", to: "analytics#index"
+        get "scheduler", to: "scheduler#index"
+        post "scheduler/pause", to: "scheduler#pause"
+        post "scheduler/unpause", to: "scheduler#unpause"
+        post "scheduler/toggle_quick_updates", to: "scheduler#toggle_quick_updates"
       end
-      member do
-        post :sync_with_spotify
-      end
-    end
-    resources :update_logs, only: [ :index ]
-    resources :share_events, only: [ :index ]
-    resources :spotify_opens, only: [ :index ]
-    resources :batch_updates, only: [ :index, :show ]
-    get "analytics", to: "analytics#index"
-    get "scheduler", to: "scheduler#index"
-    post "scheduler/pause", to: "scheduler#pause"
-    post "scheduler/unpause", to: "scheduler#unpause"
-    post "scheduler/toggle_quick_updates", to: "scheduler#toggle_quick_updates"
-  end
 
   # API endpoints for infinite scroll
   get "api/playlists", to: "api/playlists#index"
